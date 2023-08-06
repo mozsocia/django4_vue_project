cc91066c410fcb22f4d060de494f3582b1604496
Mozdalif sikder <mozsocia@gmail.com>
2023-08-06 18:11:42 +0600

    vue 2 setup complete

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,5 @@
 const mix = require('laravel-mix');
 
-mix.js('templates/firstapp/assets/js/app.js', 'static/assets/js').vue().postCss('templates/firstapp/assets/css/app.css', 'static/assets/css', [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-]);
+mix.js('templates/firstapp/assets/js/app.js', 'static/assets/js')
+    .vue();
+    // .sass('resources/sass/app.scss', 'public/css');
